e6a08c1e164b248cf7da90bba71b31233a7e3181
ImHungry48 <alaisha.barber2@gmail.com>
2024-08-26 11:05:33 -0400

    Correct logging in model and moved inde

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import logging
 from transformers import pipeline
 
 model_id = "cardiffnlp/twitter-roberta-base-sentiment-latest"
@@ -5,11 +6,14 @@ model_id = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 # Initialize the sentiment analysis pipeline
 sentiment_analyzer = pipeline('sentiment-analysis', model=model_id)
 
+# Set up logging
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def analyze_sentiment(text):
     try:
         result = sentiment_analyzer(text)
         return result
     except Exception as e:
         # Log the exception for debugging
-        print(f"Model Error: {e}")
+        logging.error(f"Model Error: {e}")
         return {'error': 'An error occurred during sentiment analysis'}
