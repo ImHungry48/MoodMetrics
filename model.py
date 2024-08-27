c7acfa3c603c5e5ed89208052aa731e3dcbeaa15
ImHungry48 <alaisha.barber2@gmail.com>
2024-08-27 09:41:35 -0400

    Result formatting

--- model.py
+++ model.py
@@ -12,8 +12,26 @@ logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %
 def analyze_sentiment(text):
     try:
         result = sentiment_analyzer(text)
-        return result
+        return format_result(result)
     except Exception as e:
         # Log the exception for debugging
         logging.error(f"Model Error: {e}")
         return {'error': 'An error occurred during sentiment analysis'}
+
+def format_result(result):
+    try:
+        label_result = result[0].get('label')
+        formatted_result = ''
+
+        print(label_result)
+        if label_result == "positive":
+            formatted_result = "Positive"
+        elif label_result == "negative":
+            formatted_result = "Negative"
+        elif label_result == "neutral":
+            formatted_result = "Neutral"
+        else:
+            raise Exception
+        return formatted_result
+    except Exception as e:
+        return {'error': 'An error occurred during result formatting'}
